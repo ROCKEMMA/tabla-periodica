4c1c8d45d46c5a6e54036862f35680b1d18bb4a5
ema <respositorio50@gmail.com>
2024-09-04 09:28:48 -0600

    header de la aplicacion

---
+++ componentes/header/header.js
@@ -0,0 +1,20 @@
+const vinculoheader = document.querySelector("header")
+
+vinculoheader.innerHTML =`
+
+
+  <div class="logo">
+            <span>Tabla Periódica 2024</span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Repositorio</a></li>
+                <li><a href="#">Integrantes</a></li>
+                <li><a href="#">Discord</a></li>
+            <a href="#" class="github-btn">GitHub</a>
+
+            </ul>
+        </nav>
+
+
+`;
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-/* const vinculo = document.querySelector("root");
+const vinculo = document.querySelector(".root");
 
 
 vinculo.innerHTML =`
 
+<header></header>
 
 
 
-
-`; */
+`; 
 
 console.log("Hola Mundo")
